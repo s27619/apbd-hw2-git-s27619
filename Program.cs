e53f20b1670b3225a75f3a38a6a9f23e7fd0a2ff
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-16 21:18:17 +0100

    update Program.cs to initialize Student and Employee and print details

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student(1, "Roman", "Klimovich");
+        Employee employee1 = new Employee(2, "Anna", "Nowak");
+
+        Console.WriteLine($"Student: {student1.firstName} {student1.lastName}, Max Rentals: {student1.MaxRentals}");
+        Console.WriteLine($"Employee: {employee1.firstName} {employee1.lastName}, Max Rentals: {employee1.MaxRentals}");
+    }
+}
