a6dfe18d90f6978b81ac901fe4adbd29ae370569
Roman Klimovich <s27619@pjwstk.edu.pl>
2026-03-18 16:24:16 +0100

    feat: presents overdue rentals and in the system

--- Program.cs
+++ Program.cs
@@ -14,11 +14,14 @@
 
         Laptop laptop = new Laptop(1, "Dell", "i7", 16);
         Projector projector = new Projector(2, "Epson", 1500, true);
+        Camera camera = new Camera(3, "Canon", 456, "EOS", true);
 
         DataStore.Equipment.Add(laptop);
         DataStore.Equipment.Add(projector);
+        DataStore.Equipment.Add(camera);
 
         service.RentEquipment(student1, laptop, 3);
+        service.RentEquipment(employee1, projector, -1);
 
         Console.WriteLine("Laptop rented successfully.");
 
@@ -30,6 +33,13 @@
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("Overdue Rentals:");
+        var overdueRentals = service.GetOverdueRentals();
+        foreach (var rental in overdueRentals)
+        {
+            Console.WriteLine($" - {rental.User.FirstName} {rental.User.LastName}: {rental.Equipment.Name}");
         }
     }
 }
